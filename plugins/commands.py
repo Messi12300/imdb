8bf8e81c71528431e2dd8861ddf63354737b0089
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2021-08-28 14:08:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -67,7 +67,7 @@ async def start(bot, cmd):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'https://t.me/newmoviesottrelease')
                     ],
                     [
                         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat='')
@@ -103,7 +103,7 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Bot Updates", url="https://t.me/Mo_Tech_YT")
+                        InlineKeyboardButton("Bot Updates", url="https://t.me/malayalammoviesdownloader")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
